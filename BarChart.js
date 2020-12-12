edcc4e19d672bd53211316dd91ed699038f0aae7
Param G <pgandhi2@wisc.edu>
2020-12-12 14:03:00 -0600

    layout using html, css and spider plot with 4 featues using js, d3

---
+++ BarChart.js
@@ -0,0 +1,12 @@
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+
+
+var svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
